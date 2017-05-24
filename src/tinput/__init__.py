6a3f91e137e96a95b262a56999d88778dc36dd50
Harshad Sharma <harshad@sharma.io>
2017-05-24 16:41:57 +0530

    Add: select_timed_input()

--- src/tinput/__init__.py
+++ src/tinput/__init__.py
@@ -1,7 +1,11 @@
 __version__ = "0.1.0"
 
-from .tinput import signal_timed_input
+from .tinput import (
+    signal_timed_input,
+    select_timed_input,
+)
 
 __all__ = [
     'signal_timed_input',
+    'select_timed_input',
 ]
--- src/tinput/tinput.py
+++ src/tinput/tinput.py
@@ -1,5 +1,9 @@
 # coding=utf-8
+from __future__ import print_function
+
+import select
 import signal
+import sys
 
 
 class TinputTimeoutError(Exception):
@@ -10,7 +14,7 @@ def _timeout_handler(signum, frame):
     raise TinputTimeoutError()
 
 
-def _get_input(prompt):
+def _get_select_input(prompt):
     try:
         return input(prompt)
     except TinputTimeoutError:
@@ -18,8 +22,27 @@ def _get_input(prompt):
 
 
 def signal_timed_input(prompt, timeout):
+    """
+    ref: 
+      - https://stackoverflow.com/a/1336751/996800
+    """
     signal.signal(signal.SIGALRM, _timeout_handler)
     signal.alarm(timeout)
-    user_input = _get_input(prompt)
+    user_input = _get_select_input(prompt)
     signal.alarm(0)
     return user_input
+
+
+def select_timed_input(prompt, timeout):
+    """
+    ref: 
+     - https://stackoverflow.com/a/2904057/996800
+     - https://stackoverflow.com/a/230774/996800
+    """
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    ready, _, _ = select.select([sys.stdin], [], [], timeout)
+    if ready:
+        return sys.stdin.readline().strip()
+    else:
+        return None
